4c4c0712ec418bade8b300de46f4a724e05e0de2
Harshavardhana Krishna Chaitanya <kccodes365@gmail.com>
2025-05-31 09:58:35 +0530

    Added an express server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,10 @@
-// const express = require("express");
-// const port = 3000;
-// const app = express();
-// app.get("/api")
+import express from "express";
+const port = 4000;
+const app = express();
+app.get("/backend/users", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
